2da589c355eadf6bfc0a94609cc9357925a11a50
tatamiya <tamyuji1002@gmail.com>
2021-09-06 20:56:37 +0900

    JavaScript basics: Supercharging our example website

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,34 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img');
 
-document.querySelector('html').onclick = function () {
-    alert('Ouch! Stop poking me!');
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox.jpeg') {
+        myImage.setAttribute('src', 'images/chrome.jpeg');
+    } else {
+        myImage.setAttribute('src', 'images/firefox.jpeg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
 }
